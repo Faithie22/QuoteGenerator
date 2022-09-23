d887b6d8230e0763ac0f2b407e7a356d38e89a78
I am Faith <104869994+Faithie22@users.noreply.github.com>
2022-09-23 21:31:29 +0100

    Add files via upload

---
+++ quote.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector("#generate");
+btn.addEventListener("click", myFunction);
+function myFunction() {
+	const quotes = {
+		"Pamela Imomo": "Never manipulate youself to compete with people.",
+		"Ghandhi": "We must be the change we want to see.",
+        "Faithie": "Always be yourself, because life is too short to be someone else.",
+        "Faithspeaks": "Perphaps we should learn to love ourselves more fiercely.",
+        "Faith Onoshirie": "If you feel good about yourself, feel confident about yourself and carry yourself with pride, that is beauty."
+	};
+	let speakers = Object.keys(quotes);
+	let aSpeaker = speakers[Math.floor(Math.random()* speakers.length)];
+	let aQuote = quotes[aSpeaker];
+    document.querySelector("#quote").textContent = aQuote;
+    document.querySelector("#speaker").textContent = aSpeaker;
+}
